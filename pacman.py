eb38a665e036d6b78e4d03b9bdbdc4001d24715b
Maksim Nazarov <alexfinsher@gmail.com>
2018-11-17 20:27:20 +0300

    Добавлены важные комментарии

--- pacman.py
+++ pacman.py
@@ -1,25 +1,46 @@
+"""
+
+    Класс Pacman
+---------------------
+
+ - Пока что сделано только отображение на пустом поле
+ - Временная реализация движения
+
+ ВАЖНО: движение по реализовано по ячейкам из
+        предположения, что одна ячейка имеет
+        размеры 16х16 пикселей. Это, возможно,
+        не окончательный вариант перемещения,
+        т.к. я пока точно не знаю, как одно
+        должно реализовываться
+
+--------------------
+
+"""
+
 import pygame
 from constants import WIDTH, HEIGHT
+
+filename = "images/tmp_pacman.png"  # !! Временная картинка Пакмана. НУЖНО ЗАМЕНИТЬ
 
 
 class Pacman:
     def __init__(self):
-        self.image = pygame.image.load("images/tmp_pacman.png")
+        self.image = pygame.image.load(filename)
         self.rect = self.image.get_rect()
         self.x = WIDTH/2 - 16
         self.y = HEIGHT/2 - 16
         self.__update_system_position()
-        self.direction = 0              # 0 - > | 1 - v | 2 - < | 3 - ^
-        # self.cash
+        self.direction = 0      # 0 - > | 1 - v | 2 - < | 3 - ^  -- направления движения
+        # self.cash - заготовка для кеширования направления движения
 
     def __update_system_position(self):
         self.rect.x = self.x
         self.rect.y = self.y
 
     def check_event(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                self.direction = 2
+        if event.type == pygame.KEYDOWN:        # !! Здесь ещё нет проверки на возможные
+            if event.key == pygame.K_a:         # пересечения с другими объектами, т.к.
+                self.direction = 2              # их классов ещё нет
             if event.key == pygame.K_d:
                 self.direction = 0
             if event.key == pygame.K_s:
@@ -32,8 +53,8 @@ class Pacman:
             self.x += 16
             self.__update_system_position()
         if self.direction == 1 and self.y+16 < HEIGHT:
-            self.y += 16
-            self.__update_system_position()
+            self.y += 16                                # !! Здесь также пока нет
+            self.__update_system_position()             # проверки на коллизию
         if self.direction == 2 and self.x > 0:
             self.x -= 16
             self.__update_system_position()
