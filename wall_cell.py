54ff3ce892958f4d098166cce4743dfd12065aa3
Nadia Panchenko <nadja.panchenko@gmai.com>
2018-11-19 09:58:20 +0300

    Added WallCell class

---
+++ wall_cell.py
@@ -0,0 +1,8 @@
+from cell import Cell
+
+class WallCell(Cell):
+    def __init__(self, x, y, cell_size):
+        Cell.__init__(self, x, y, cell_size)
+        self.color = 70, 130, 180
+        self.fill = 0
+        self.walkable = False
