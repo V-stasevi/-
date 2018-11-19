cd94bcd71f87f91ce412e2f2cfc6cdb33c50a865
Nadia Panchenko <nadja.panchenko@gmai.com>
2018-11-19 09:56:08 +0300

    Added cell class

---
+++ cell.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Cell:
+
+    def __init__(self, x, y, cell_size=9):
+        self.x = x
+        self.y = y
+        self.color = 0,0,0
+        self.fill = 0
+        self.walkable = True
+        self.cell_size = cell_size
+        self.width = cell_size
+        self.height = cell_size
+
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, (self.x, self.y, self.width, self.height), self.fill)
+        #print("drawing individual cell")
