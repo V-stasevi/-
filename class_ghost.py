3adf797885b7c1b26b69299aeb1817cd6b04cedb
Vallery Maximovich <torm.kira@yandex.ru>
2018-12-08 01:22:44 +0300

    Добавлены функции коллизий гостов с пакманом, и ф-я is_dead для пакмана

--- class_ghost.py
+++ class_ghost.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import picGhost_Dead, up, right, down, left
+from pacman import Pacman
 
 common = True
 
@@ -38,19 +39,22 @@ class Ghost:
     def logic(self, window_width, window_height):
         self.__update_system_position()
 
-    # def is_dead(self):
-    #     self.image = pygame.image.load(picGhost_Dead)
-    #     if " pacman eat ghost ":                        # здесь нужно вставить условия, что съели приведение
-    #         self.direction = up
-    #         self.x = 15 * 16 - 16
-    #         self.y = 15 * 16
-    #         self.__update_system_position()
-    #
+    def is_dead(self):
+        self.image = pygame.image.load(picGhost_Dead)
+        if self.pinky.rect.colliderect(Pacman.rect) and self.state != common:            #pacman.rect нужно нормльно привязать
+            self.direction = up
+            self.x = 15 * 16 - 16
+            self.y = 15 * 16
+            self.__update_system_position()
+
     # def collision(self, image):
-    #     if self.pinky.rect.colliderect(self.pacman.rect):
+    #     if self.pinky.rect.colliderect(Pacman.rect) and Pacman.state == common:
     #         pass
     #         where pacman dies
     #         or
     #         dies ghost: self.is_dead()
     #         self.image = image()
 
+    def collosion(self):
+        if self.pinky.rect.colliderect(Pacman.rect) and self.state == common:
+            Pacman.is_dead()                                                    #pacman.rect и pacman.is_dead!
