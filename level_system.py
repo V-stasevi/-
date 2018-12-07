84debc5fb720e78bdba720b4b7df1335fb8aee5f
Линник Игорь <linnigor@mail.ru>
2018-12-07 23:08:54 +0300

    Add new file

---
+++ level_system.py
@@ -0,0 +1,23 @@
+import pygame
+from settings_control  import _append_buttons
+
+class level_system:
+
+
+    def __init__(self, _append_buttons):
+        self.speps=0;
+        self.level= _append_buttons;    # неуверен ту ли функцию подключил, но другой не нашёл
+    
+    
+    def speed(self):            # Вроде функция должа возращать скорость перемещения
+        if self.level== 1:
+            self.speeds= 10
+            return self.speeds
+        elif self.level== 2:
+            self.speeds= 20
+            return self.speeds
+        else self.level== 3:
+            self.speeds= 30
+            return self.speeds
+        
+        
